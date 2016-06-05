37dec8d76c17a1144d232596fc83a88f3a05c483
Travis Johnson <travisjohnson@T-Ravs-Mac.local>
2016-06-05 16:33:40 -0400

    BREAKTHROUGH

--- assets/js/mg.js
+++ assets/js/mg.js
@@ -10,23 +10,63 @@
 
 $('.card').on('click', function () {
   $(this).addClass("rotate");
-  if  ("div:contains('1')" != "div:contains('2')"){
-     setTimeout(function(){ $('.front').addClass("rotate") }, 2000);
+  var flipped = $('.rotate')
+  if  (flipped.length <= 2){
+      var firstCard = flipped.first();
+      var secondCard = flipped.last();
+  {
+  if (firstCard.text() != secondCard.text()) {
+     setTimeout(function(){ $('.card').toggleClass("rotate") }, 500);
 
     console.log('aw snap!')
  }
+ }
+ }
  });
-    // $(this).addClass("rotate");
-//   var flipped = $('.rotate');
-//   if (flipped.length <= 2){
-//     var firstCard = flipped.first();
-//     var secondCard = flipped.last();
-// {
+
+
+var minTime = document.getElementById("minute");
+var secTime = document.getElementById("seconds");
+var total = 0;
+setInterval(setTime, 1000);
+
+function setTime()
+{
+  ++total;
+  secTime.innerHTML = pad(total%60);
+  minTime.innerHTML = pad(parseInt(total/60));
+}
+
+function pad(val)
+{
+  var valString = val + "";
+  if (valString.length < 2)
+{
+  return "0" + valString;
+}
+else
+{
+    return valString;
+  }
+};
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+//if you click the first card then click the second and the second does not match
+//flip over the second card.
 
 
 
@@ -44,4 +84,4 @@ $('.card').on('click', function () {
 
 
 // if ( "div:contains('camera')" ).toggleClass( 'rotate' );
-// console.log('shit works!!!')
+// console.log('ish works!!!')
